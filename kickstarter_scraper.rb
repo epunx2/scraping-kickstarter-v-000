63df35e4b41725732c7b421a6981f2f024526bfd
Eric Chrobak <eric@ericchrobak.com>
2018-06-18 23:26:10 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,14 +9,14 @@ def create_project_hash
   projects = {}
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    projects[title.to_sym] = {}
+    projects[title.to_sym] = {
+      :image_link => project.css("div.project-thumbnail a img").attribute("src").value,
+      :description => project.css("p.bbcard_blurb").text,
+      :location => project.css("ul.project-meta span.location-name").text,
+      :percent_funded => project.css("ul.project-stats li.first.funded strong").text.gsub("%","").to_i
+    }
   end
-  # project = kickstarter.css("li.project.grid_4")
-  # title: project.css("h2.bbcard_name strong a").text
-  # image link: project.css("div.project-thumbnail a img").attribute("src").value
-  # description: project.css("p.bbcard_blurb").text
-  # Location: project.css("ul.project-meta li a span.location-name").text
-  # % Funded: project.css("ul.project-stats li.first.funded strong").text.gsub("%", "").to_i
+
   projects
 end
 create_project_hash
