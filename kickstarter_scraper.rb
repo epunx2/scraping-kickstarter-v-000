7968179caf72ded24b9ff34a1232a51201732258
Eric Chrobak <eric@ericchrobak.com>
2018-06-18 23:20:53 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -11,6 +11,7 @@ def create_project_hash
   # title: project.css("h2.bbcard_name strong a").text
   # image link: project.css("div.project-thumbnail a img").attribute("src").value
   # description: project.css("p.bbcard_blurb").text
-  # project.css("ul.project-meta li.first a span.location-name").text
+  # Location: project.css("ul.project-meta li a span.location-name").text
+  # 
 end
 create_project_hash
